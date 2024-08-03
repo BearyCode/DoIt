05bc0ef855cdcb40c8242cf8d5e82fba8a560c08
BearyCode <rettingerm@protonmail.com>
2024-08-03 12:14:24 +0200

    Initial Commit

---
+++ DoIt/DoItApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoItApp.swift
+//  DoIt
+//
+//  Created by BearyCode on 03.08.24.
+//
+
+import SwiftUI
+
+@main
+struct DoItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
